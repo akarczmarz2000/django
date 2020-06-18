b2ae012dd0df3d20dea0fe2461041c86a90214bf
Armand Józef Oliver Karczmarz <akarczmarz2000@gmail.com>
2020-06-18 02:34:31 +0000

    Requirements and setting update for delpoyment

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.parse("postgres://lmhimlwsbixpcr:adcdf838390c1041014096dfc867c8be8bf694f4867480a8db22eef0e67c7b36@ec2-54-75-246-118.eu-west-1.compute.amazonaws.com:5432/daeqeh81237p2f")
 }
 
 
